7610ebe71d57771cd5c606723060f3085846a7fc
hyomin1 <98298940+hyomin1@users.noreply.github.com>
2022-07-01 23:02:49 +0900

    CatEscape

--- GameDirector.cs
+++ GameDirector.cs
@@ -5,26 +5,16 @@ using UnityEngine.UI;
 
 public class GameDirector : MonoBehaviour
 {
-    GameObject car;
-    GameObject flag;
-    GameObject distance;
+    GameObject hpGauge;
     // Start is called before the first frame update
     void Start()
     {
-        this.car = GameObject.Find("car");
-        this.flag = GameObject.Find("flag");
-        this.distance = GameObject.Find("Distance");
+        this.hpGauge = GameObject.Find("hpGauge");
     }
 
     // Update is called once per frame
-    void Update()
+    public void DecreaseHp()
     {
-        float length = this.flag.transform.position.x - this.car.transform.position.x;
-        if (length >= 0)
-            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m";
-        else
-            this.distance.GetComponent<Text>().text = "게임 오버!";
-
-
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
     }
 }
